50d58eb70d31aae6f59bfd398d210da6f7b234d3
VitySilva <vitoria191200@gmail.com>
2025-10-31 20:48:28 -0300

    Update recursividade2.py

--- recursividade2.py
+++ recursividade2.py
@@ -1,9 +1,9 @@
 def busca_binaria(lista, valor, inicio, fim):
-    if inicio > fim:  
+    if inicio > fim: 
         return -1
     meio = (inicio + fim) // 2
 
-    if lista[meio] == valor:  
+    if lista[meio] == valor:
         return meio
     elif valor < lista[meio]:  
         return busca_binaria(lista, valor, inicio, meio - 1)
@@ -11,6 +11,16 @@ def busca_binaria(lista, valor, inicio, fim):
         return busca_binaria(lista, valor, meio + 1, fim)
 
 
-valores = [1, 3, 5, 7, 9, 11]
-resultado = busca_binaria(valores, 7, 0, len(valores) - 1)
-print("Índice do valor 7:", resultado)
+print("=== BUSCA BINÁRIA RECURSIVA ===")
+
+
+entrada = input("Digite os números da lista em ordem crescente, separados por espaço: ")
+lista = [int(x) for x in entrada.split()]
+valor = int(input("Digite o valor que deseja procurar: "))
+
+resultado = busca_binaria(lista, valor, 0, len(lista) - 1)
+
+if resultado != -1:
+    print(f"O valor {valor} foi encontrado no índice {resultado}.")
+else:
+    print(f"O valor {valor} não foi encontrado na lista.")
